8b892ce1bc7041ee5f42afa807c987d53d8d4845
Deckins <bennythai2013@hotmail.com>
2019-03-31 00:00:13 -0400

    Merged kevin's changes

--- project/src/index.js
+++ project/src/index.js
@@ -5,9 +5,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const Apps = (
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
+ReactDOM.render(Apps, document.getElementById('root'));
 
 serviceWorker.unregister();
