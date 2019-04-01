f966ee6afb790e7770cbd218dc8241b9363f7c24
Deckins <bennythai2013@hotmail.com>
2019-04-01 01:12:14 -0400

    Added Charts to simulate dummy data

--- project/src/Data/Charts.js
+++ project/src/Data/Charts.js
@@ -1,25 +1,47 @@
 import React, {Component} from 'react';
-import {Bar} from 'react-chartjs-2';
+import {Bar, Doughnut} from 'react-chartjs-2';
 
 
 class Charts extends Component {
     state={
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            labels: ["January", "February", "March" ],
             datasets: [{
-                label: "Total expenses for each month",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 100],
+                label: "My First dataset",
+                data: [ 81, 56, 55, 40],
+                backgroundColor: ["#FF6384",
+                    "#63FF84",
+                    "#8463FF",
+                    "#6384FF"],
             }]
+        },
+        pie : {
+            labels: [
+                "Food",
+                "Utilities",
+                "Transporation",
+                "Entertainment"
+            ],
+            datasets: [
+                {
+                    data: [133.3, 86.2,  51.2, 50.2],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#63FF84",
+                        "#8463FF",
+                        "#6384FF"
+                    ]
+                }]
         }
     }
 
     render() {
         return (
             < div >
+                <Doughnut data={this.state.pie} />
                 < Bar data={this.state.data} />
-            < /div>
+
+        < /div>
     )
         ;
     }
