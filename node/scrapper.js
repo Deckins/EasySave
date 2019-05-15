69a5ad8d350b29ad15ebb9f21d57ddc5d6b54f0e
Deckins <bennythai2013@hotmail.com>
2019-05-15 04:19:32 -0400

    Added express

--- node/scrapper.js
+++ node/scrapper.js
@@ -1,5 +1,8 @@
 const request = require('request');
 const cheerio = require('cheerio');
+var express = require('express');
+var app = express();
+const port = 3000;
  
 request('https://www.expatistan.com/cost-of-living/new-york-city', (error,response,html) => {
     if(!error && response.statusCode==200){
@@ -17,8 +20,15 @@ request('https://www.expatistan.com/cost-of-living/new-york-city', (error,respon
             if(obj.name && obj.price)
             console.log('Expense: ' + obj.name + '\nPrice: ' + obj.price + '\n')
         })   
+        app.get('/', function (req, res) {
+            res.send(foodObj)
+          })
     }
     else
         console.log(error)
 })
 
+
+// respond with "hello world" when a GET request is made to the homepage
+
+app.listen(port, () => console.log(`Example app listening on port ${5000}!`))
