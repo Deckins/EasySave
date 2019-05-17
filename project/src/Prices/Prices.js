90a0409a32a501cbb5d9c96c62d47059625ec079
Deckins <bennythai2013@hotmail.com>
2019-05-16 20:10:55 -0400

    Prices page now displays all names and prices

--- project/src/Prices/Prices.js
+++ project/src/Prices/Prices.js
@@ -1,27 +1,63 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-class Prices extends Component {
-    state={
-        pricesObj:[]
-    }
-    componentDidMount(){
-        axios.get('http://localhost:5000/')
-        .then(response => {
-            console.log(response.data)
-            this.setState({pricesObj:response.data})
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+import PriceDisplay from '../PriceDisplay/PriceDisplay';
+import './Prices.css';
 
-        })
+class Prices extends Component {
+    state = {
+        pricesObj: [
+            [{name:'bob',price:'16'}],
+            [{name:'bob',price:'16'}],
+            [{name:'bob',price:'16'}],
+            [{name:'bob',price:'16'}],
+            [{name:'bob',price:'16'}]
+        ],
+        loading: true
     }
+    componentDidMount() {
+        axios.get('http://localhost:5000/')
+            .then(response => {
+                this.setState({ pricesObj: response.data })
+
+            })
+            .catch(error => console.log(error))
+            this.setState({ loading: false })
+    }
+
     render() {
+        
+        let output = null
+        if(this.state.loading){
+           output = <div class="lds-circle"><div></div></div>
+        }
+        else if(!this.state.loading){
+            output = (
+                <div>
+                      {this.state.pricesObj.map(obj => (
+                           <TabPanel>
+                                {obj.map(o => 
+                                    <PriceDisplay price={o.price} name={o.name}/>
+                                )}
+                           </TabPanel> 
+                      ))}
+                </div>
+            )
+        }
         return (
             <div>
-                {this.state.pricesObj.map(obj =>{
-                    // if(obj.name && obj.price)
-                    return obj.map(o =>
-                         <p>{o.name} and {o.price}</p>
+                <Tabs>
+                    <TabList>
+                        <Tab>Food</Tab>
+                        <Tab>Housing</Tab>
+                        <Tab>Clothing</Tab>
+                        <Tab>Travel</Tab>
+                        <Tab>Utilities</Tab>
+                    </TabList>
+                    {output}
+                </Tabs>
 
-                    )
-                })}
             </div>
         );
     }
