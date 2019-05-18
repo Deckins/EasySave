5010d49e4e6b1d06170262d0d0ac3494c6b84ba9
KevinJHuang7 <kevinhuangj7@yahoo.com>
2019-05-18 16:32:05 -0400

    Updated Home Files

--- project/src/Home/Home.js
+++ project/src/Home/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import piggy from '../images/piggybank.jpg';
+import piggybank from '../images/piggy.jpg';
+import ben100 from '../images/ben100.jpg';
+import your_expenses from '../images/your_expenses.jpg';
 import './Home.css';
 import { Card, CardDeck } from 'react-bootstrap';
 
@@ -21,70 +24,74 @@ class Home extends Component {
                 <p className='statement'> Save Easily </p>
                 </div>
 
-                <section>
+                <section className= 'section'>
                 <div className= 'divider'>
 
                 {/*There will be 3 cards below that will be used to attract the users to
-                  wanrt to know more about EasySave.  Little details will be used, and
+                  want to know more about EasySave.  Little details will be used, and
                   will get the users to want to learn more.*/}
-                <CardDeck>
-                <Card border='success' bg="light" style={{ width: '20rem' }}>
-                <span class='card1h'>
-                <Card.Header >Retirement</Card.Header>
-                </span>
 
-                {/*The first little detail in the card*/}
-                <span class='card1'>
-                <Card.Body>
-                <Card.Title>Are you Ready?</Card.Title>
-                <Card.Text>
-                Its never too early to think about retirement. Are you on track to a
+                {/*The first little detail in the card.
+                  EasySave will attract users by aiding them before retirement.
+                  In fact, we are starting now so retirement will be easy for the users.*/}
+                <div class='card'>
+                <div class='image1'>
+                <img className='piggybank' src={piggybank} alt='Here is a piggybank'/>
+                </div>
+                <div className='contain'>
+                <div class='title1'>
+                <h1 className= "titlename1"> Retirement</h1>
+                </div>
+                <div class='des1'>
+                <p> Its never too early to think about retirement. Are you on track to a
                 secure future? We can help you find out!
-                </Card.Text>
-                </Card.Body>
-                </span>
-                </Card>
+                <br/> <br/> <br/>
+                </p>
+                </div>
+                </div>
+                </div>
 
-
-                <Card border='success' bg="light" style={{ width: '20rem' }}>
-                <span class='card2h'>
-                <Card.Header >Expenses</Card.Header>
-                </span>
-
-                {/*The second little detail in the card*/}
-                <span class='card2'>
-                <Card.Body>
-                <Card.Title>Concerned about Debt?</Card.Title>
-                <Card.Text>
-                Do you know how much you're spending on each month? Start calculating now and take control
+                {/*The second little detail in the card.
+                  EasySave will attract users by helping them check their finances.*/}
+                <div class='card'>
+                <div class='image2'>
+                <img className='your_expenses' src={your_expenses} alt='Here is your expense'/>
+                </div>
+                <div className='contain'>
+                <div class='title2'>
+                <h1 className= "titlename2"> Expenses </h1>
+                </div>
+                <div class='des2'>
+                <p> Do you know how much you're spending on each month? Start calculating now and take control
                 of your financial future.
-                </Card.Text>
-                </Card.Body>
-                </span>
-                </Card>
+                <br/> <br/> <br/>
+                </p>
+                </div>
+                </div>
+                </div>
 
-
-                <Card border='success' bg="light" style={{ width: '20rem' }}>
-                <span class='card3h'>
-                <Card.Header >Success</Card.Header>
-                </span>
-
-                {/*The third little detail in the card*/}
-                <span class='card3'>
-                <Card.Body>
-                <Card.Title>Do you want to accumulate Wealth?</Card.Title>
-                <Card.Text>
-                You'd be surprised how much you can save by setting aside money each week. Start setting goals now
+                {/*The third little detail in the card.
+                  Users would not know how much money should be saved.
+                  EasySave will help users find out how much money should be saved,
+                  in order for users to be able to spend money on the things they want. */}
+                <div class='card'>
+                <div class='image3'>
+                <img className='ben100' src={ben100} alt='Here is a Benjamen Franklin'/>
+                </div>
+                <div className='contain'>
+                <div class='title3'>
+                <h1 className= "titlename3"> Success </h1>
+                </div>
+                <div class='des3'>
+                <p> You'd be surprised how much you can save by setting aside money each week. Start setting goals now
                 and finally get that new dinner table or flatscreen tv you've always wanted!
-                </Card.Text>
-                </Card.Body>
-                </span>
-                </Card>
-                </CardDeck>
+                </p>
+                </div>
+                </div>
+                </div>
+
                 </div>
                 </section>
-
-                <br /> <br />
 
                 </div>
               </div>
