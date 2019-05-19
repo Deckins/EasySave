4254fa21ffecd01d277d1d2a83598286a51fdffd
Deckins <bennythai2013@hotmail.com>
2019-05-19 17:41:55 -0400

    prices filter

--- project/src/Prices/Prices.js
+++ project/src/Prices/Prices.js
@@ -5,7 +5,6 @@ import "react-tabs/style/react-tabs.css";
 import PriceDisplay from '../PriceDisplay/PriceDisplay';
 import './Prices.css';
 
-{/*Thr Price page, will display the price and how much you spend on certain items*/}
 class Prices extends Component {
     state = {
         pricesObj: [
@@ -15,21 +14,44 @@ class Prices extends Component {
             [{name:'bob',price:'16'}],
             [{name:'bob',price:'16'}]
         ],
-        loading: true
+        pricesOrg:[],
+        loading: true,
+        priceSort: 0
     }
     componentDidMount() {
         axios.get('https://ezsave-5c146.firebaseio.com/PricesData/-Lf8nUUS1Y9MoQiKaaBy/Prices.json')
             .then(response => {
-                // console.log(response.data)
+                //The original array is there to keep the original state of the prices array 
+                //have to use .slice(0) to make a copy not reference
                 this.setState({ pricesObj: response.data })
-
+                const copyArray = this.state.pricesObj.slice(0)
+                this.setState({ pricesOrg: copyArray })
             })
             .catch(error => console.log(error))
             this.setState({ loading: false })
     }
+    handleChange = (event) => {
+        this.setState({priceSort:event.target.value}); 
+        console.log('this is the intial value after input', this.state.priceSort)       
+        this.setState({pricesObj:this.state.pricesOrg})
+        console.log('this is trying to set the org prices:', this.state.pricesObj, 'to' , this.state.pricesOrg)
+        let sortedPricesObj = this.state.pricesOrg[0].filter(obj => {
+                //This is to remove the intial $ at the beginning of the prices
+                let numb = obj.price.substring(1,obj.price.length)
+                // console.log(' prices:', numb,'price entered: ', this.state.priceSort)
+                return parseInt(numb,10) <= event.target.value
+           
+        })  
+
+        console.log(sortedPricesObj)
+        const copyArr= this.state.pricesObj
+        copyArr[0] = sortedPricesObj
+        this.setState({pricesObj:copyArr});
+       
+    }
 
     render() {
-
+        
         let output = null
         if(this.state.loading){
            output = <div class="lds-circle"><div></div></div>
@@ -39,16 +61,18 @@ class Prices extends Component {
                 <div>
                       {this.state.pricesObj.map(obj => (
                            <TabPanel>
-                                {obj.map(o =>
-                                    <PriceDisplay price={o.price} name={o.name}/>
-                                )}
-                           </TabPanel>
+                                {obj.map(o => <PriceDisplay price={o.price} name={o.name}/> )}
+                           </TabPanel> 
                       ))}
                 </div>
             )
         }
         return (
-            <div className = "tabs">
+            <div>
+                <label> 
+                    <input type="text"   onChange={this.handleChange} />
+        
+                </label>
                 <Tabs>
                     <TabList>
                         <Tab>Food</Tab>
@@ -59,7 +83,6 @@ class Prices extends Component {
                     </TabList>
                     {output}
                 </Tabs>
-
             </div>
         );
     }
